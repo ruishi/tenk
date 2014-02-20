9af8d521635a6ff4ccb53f0ca69505e9ecc37df1
ruishi <ganruishi@gmail.com>
2014-02-19 19:53:47 -0500

    fix unit tests in Windows
    Had a test that failed in Win7 but not in Ubuntu. It turns on that on Windows when you run a testsuite different test classes can share state. Wrote a setUp() and tearDown() method in tkserializer_test to fix the failing test in users_test

--- tests/tkserializer_test.py
+++ tests/tkserializer_test.py
@@ -11,18 +11,25 @@ from users import User
 import json
 
 class TKSerializerTest(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.user = User("rdg")
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        del self.user.skillset[:]
+
     def test_serializer(self):
         """serializer should successfully convert from 
         custom classes to json and vice versa"""
-        user = User("rdg")
-        user.add_skill("programming", 50)
-        user.add_skill("chinese", 200)
+        self.user.add_skill("programming", 50)
+        self.user.add_skill("chinese", 200)
         with open('test.tk', encoding='utf-8', mode='w') as f:
-            json.dump(user, f, default=tkserializer.to_json, indent=2)
+            json.dump(self.user, f, default=tkserializer.to_json, indent=2)
         with open('test.tk', encoding='utf-8', mode='r') as f:
             user2 = json.load(f, object_hook=tkserializer.from_json)
         os.remove('test.tk')
-        self.assertEqual(user, user2)
+        self.assertEqual(self.user, user2)
        
 
 if __name__ == '__main__':
