5f9f962500daf718fca4bada9176b73a9aec1395
ruishi <ganruishi@gmail.com>
2014-05-28 11:35:59 -0400

    update leveling system
    tenk.skills.py: change from 10 levels to a saner 100 levels. 1000 hours
    to level up was a bit much.
    tenk.skills.py: adjust calcprogress method to reflect level change.
    tests.skills_test.py adjust calclevel tests to reflect level change and
    add unit tests for calcprogress

--- tenk/skills.py
+++ tenk/skills.py
@@ -1,8 +1,4 @@
-################################################################################
-#author: RD Galang
-#desc: class and methods for skills. 
-#TODO: -create improved, sensical leveling system
-################################################################################
+"""Manages hours and level for a skill."""
 
 class OutOfRangeError(ValueError): pass
 
@@ -16,35 +12,40 @@ class Skill():
             raise OutOfRangeError("hours should be positive")
 
     def calclevel(self):
-        """Calculates the "level" a user is at for the skill. 
-        
-        Keyword arguments: None
+        """Calculates the "level" a user is at for the skill. There are 100
+        levels.
 
-        Returns int"""
+        Returns an int.
+        """
 
         level = 0
-        levels = range(0,11) #levels 0-10
+        levels = range(0,101) # levels 0-100
+        level_hours = 100 # the number of hours it takes to complete a level
         for i in levels:
-            max = (i + 1) * 1000
+            max_hours = (i + 1) * level_hours
             level = i
-            if self.hours < max:
+            if self.hours < max_hours:
                 return level
+        # if this part executes, then that means the user has exceeded
+        # 10K hours and is already at level 100. So just return level,
+        # which is set to 100 at this point.
         return level
 
     def calcprogress(self):
         """Calculates the progress being made in a certain skill. Progress
-        is determined for the user's level in the skill, to improve
+        is determined for the user's current level in the skill, to improve
         morale and progress reports.
 
         Keyword arguments: None
 
         Returns: Float"""
-
+        if self.hours >= 10000:
+            return 100
         level = self.calclevel()
-        levelmax = 1000 * (level + 1)
-        return (self.hours/levelmax) * 100        
+        level_hours = 100
+        return self.hours - (level_hours * level)
 
-    """Comparison methods for testing"""
+    # Comparison methods for testing
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
