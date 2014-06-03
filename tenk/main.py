e4671bc07894f2343a55bd592cbacc68e81672e9
ruishi <ganruishi@gmail.com>
2014-06-02 23:36:18 -0400

    replace menu-based interface
    Ripped out the menu interface in favor of a traditional CLI. I
    was originally going to keep both as the menu interface can be rather
    convenient; however, I decided I didn't want to maintain both.
    
    The new interface is influenced by todo.txt's CLI, where to modify any
    todo you enter its number.

--- tenk/main.py
+++ tenk/main.py
@@ -1,132 +1,129 @@
-"""CLI menu interface"""
+"""CLI interface for tenk"""
 
 import os
 from sys import exit
-from tenk.users import User
-import tenk.tkserializer as tkserializer
 import json
 import glob
+import argparse
+import functools
 
-def main():
-    """"Command-line menu interface entry point for tenk."""
+from tenk.users import User
+import tenk.tkserializer as tkserializer
 
-    u_choice = menu('u')
+no_data_msg = "You have no data!"
 
-    if u_choice == 'create':
-        username = input("Desired username: ")
-        user = User(username)
-        print("\nWelome {}!\n".format(username))
-        s_choice = menu('ms')
-    elif u_choice == 'load':
-        u_file = menu('l')
-        check_storage()
-        with open(u_file, encoding='utf-8', mode='r') as f:
-            user = json.load(f, object_hook=tkserializer.from_json)
-        print("\nWelcome back {}!\n".format(user.name))
-        s_choice = menu('ms')
+def get_data_path():
+    """Returns path to user's data file if it exists. Otherwise returns
+    None"""
+    data_paths = glob.glob(os.path.join(os.path.expanduser('~'),
+                                        'tenk/*.tk'))
+    if data_paths:
+        return data_paths[0]
     else:
-        exit(0)
+        return None
 
-    while s_choice != "exit":
-        if s_choice == 'add_skill':
-            skill = input("Skill name: ")
-            try:
-                hours = float(input("Initial times (hours): "))
-                user.add_skill(skill, hours)
-            except ValueError:
-                print("a valid number must be entered")
-                exit(1)
-        elif s_choice == 'delete':
-            skill = menu('s', user)
-            user.remove_skill(skill)
-        elif s_choice == 'add_time':
-            skill = menu('s', user)
-            try:
-                hours = float(input("Number of hours to add: "))
-                user.add_time(skill, hours)
-            except ValueError:
-                print("a valid number must be entered")
-                exit(1)
-        elif s_choice == 'print':
-            user.printprogress()
-            print()
-
-        s_choice = menu('ms')
-
-    check_storage()
-    u_file = os.path.join(os.path.expanduser('~'),
-                          'tenk/{}.tk'.format(user.name))
+def save(user):
+    u_file = get_data_path()
     with open(u_file, encoding='utf-8', mode='w') as f:
         json.dump(user, f, indent=2, default=tkserializer.to_json)
-        exit(0)
 
-def check_storage():
-    """Checks if there's a tenk folder in the user's home directory.
-    Creates one if one doesn't exist."""
-    if not os.path.exists(os.path.join(os.path.expanduser('~'), 'tenk')):
-        os.makedirs(os.path.join(os.path.expanduser('~'), 'tenk'))
-
-def menu(key, user=None):
-    """Creates requested menu and returns user's choice.
-
-    Keyword arguments:
-    key -- menu identifier
-
-    Returns str or None."""
-
-    user_menu_dict = {'1': 'create', '2': 'load', '3': 'exit'}
-    skill_menu_dict = {'1': 'add_skill', '2':'delete', '3':'add_time',
-                       '4':'print', '5':'exit'}
-
-    if key == 'u': #user menu
-        print("Please select an option:")
-        print("1. Create user")
-        print("2. Load user")
-        print("3. Exit")
-
-        return choose_from(user_menu_dict)
-    elif key == 'ms': #modify skill menu
-        print("1. Add skill")
-        print("2. Delete skill")
-        print("3. Add time")
-        print("4. Print progress")
-        print("5. Exit")
-
-        return choose_from(skill_menu_dict)
-    elif key == 's': #skill menu
-        skill_names = user.getskillnames()
-        skill_choices_dict = generate_dict(skill_names)
-        print("Please select an option:")
-        for i in range(len(skill_names)):
-            print("{0}. {1}".format(i + 1, skill_names[i]))
-        return choose_from(skill_choices_dict)
-    elif key == 'l': #load user menu
-        user_list = glob.glob(os.path.expanduser('~/tenk/*.tk'))
-        if not user_list:
-            print("No user data found.")
-            exit(1)
-        elif len(user_list) == 1:
-            return user_list[0]
-        else:
-            user_choice_dict = generate_dict(user_list)
-            print("Please select an option:")
-            for i in range(len(user_list)):
-                print("{0}. {1}".format(i + 1, user_list[i][:-3]))
-
-            return choose_from(user_choice_dict)
-
-def choose_from(choice_dict):
-    """Given a dictionary of menu choices, returns a choice."""
-    choice = input()
-    if choice in choice_dict:
-        return choice_dict[choice]
+def load_user(create=False):
+    """Load a user's tk file into a User object. If create is True,
+    then a user object will be created if no tk file exists."""
+    if os.path.exists(os.path.join(os.path.expanduser('~'), 'tenk')):
+        u_file = get_data_path()
+        with open(u_file, encoding='utf-8', mode='r') as f:
+            user = json.load(f, object_hook=tkserializer.from_json)
+        return user
     else:
-        print("Not a valid option.")
+        os.makedirs(os.path.join(os.path.expanduser('~'), 'tenk'))
+        if create:
+            u_file = os.path.join(os.path.expanduser('~'),
+                                  'tenk/default.tk')
+            open(u_file, 'a').close()
+            return User("Default")
+        else:
+            return None
 
 def generate_dict(xs):
     """Creates a dictionary where the key is a string representing
     a number and the value is an element of xs"""
-    return {str(i + 1):xs[i] for i in range(len(xs))}
+    return {float(i + 1):xs[i] for i in range(len(xs))}
+
+def add_time(choice, time):
+    user = load_user(create=False)
+    if user:
+        skill = generate_dict(user.get_skill_names())[choice]
+        user.add_time(skill, time)
+        save(user)
+    else:
+        print(no_data_msg)
+
+def add_skill(name, hours=0):
+    user = load_user(create=True)
+    user.add_skill(name, hours)
+    save(user)
+
+def remove_skill(choice):
+    user = load_user(create=False)
+    if user:
+        skill = generate_dict(user.get_skill_names())[choice]
+        user.remove_skill(skill)
+        save(user)
+    else:
+        print(no_data_msg)
+
+def list_skills():
+    user = load_user()
+    if user:
+        for idx, skill in enumerate(user.get_skill_names()):
+            print("{}. {}".format(idx + 1, skill))
+    else:
+        print(no_data_msg)
+
+def print_progress():
+    """Print a user's progress"""
+    user = load_user()
+    if user:
+        user.print_progress()
+    else:
+        print(no_data_msg)
 
 if __name__ == '__main__':
-    main()
+    parser  = argparse.ArgumentParser(prog="python3 -m tenk.main")
+    parser.add_argument('-l', '--ls',
+                        action='store_true',
+                        help="List skillset")
+    parser.add_argument('-s', '--skill',
+                        nargs='+',
+                        metavar=('name', 'hours'),
+                        help="Add skill to skillset")
+    parser.add_argument('-t', '--time',
+                        nargs=2,
+                        type=float,
+                        metavar=('skill', 'time'),
+                        help=("Add time to a skill"))
+    parser.add_argument('-d', '--delete',
+                        type=float,
+                        metavar='skill',
+                        help="Delete a skill")
+    parser.add_argument('-p', '--progress',
+                        action='store_true',
+                        help="Print progress")
+    args = parser.parse_args()
+
+    if args.ls:
+        list_skills()
+    elif args.progress:
+        print_progress()
+    elif args.time:
+        add_time(args.time[0], float(args.time[1]))
+    elif args.skill:
+        if len(args.skill) == 2:
+            add_skill(args.skill[0], args.skill[1])
+        else:
+            add_skill(args.skill[0])
+    elif args.delete:
+        remove_skill(args.delete)
+    else:
+        parser.print_help()
