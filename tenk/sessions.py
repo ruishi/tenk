87224a5bf45905c619030253bc24cd072f73b90f
ruishi <ganruishi@gmail.com>
2014-06-07 23:20:24 -0400

    add session update functionality
    tenk/sessions.py: If there already exists session data for a given date,
    the new data will be "merged" with the old data. Specifically, hours
    will be added together and any extra notes will be appended to any old
    notes. Methods find_existing_session and update_xml were created for
    this functionality.

--- tenk/sessions.py
+++ tenk/sessions.py
@@ -9,13 +9,16 @@ class Session:
 
     Attributes:
     skill_name (str) -- name of the skill being practiced
-    hours (float) -- the amount of hours spent on the practice session
+    hours (float) -- optional, the amount of hours spent on the
+                     practice session
     session_date (datetime.date) -- the date of the practice session
     practiced (str) -- optional, what was practiced
     improved (str) -- optional, what improved from last time
-    future (str) -- optional, what to focus on in future sessions"""
+    future (str) -- optional, what to focus on in future sessions
 
-    def __init__(self, skill_name, hours, session_date=None,
+    """
+
+    def __init__(self, skill_name, hours=None, session_date=None,
                  practiced=None, improved=None, future=None,
                  file_path=None):
         self.skill_name = skill_name
@@ -24,12 +27,9 @@ class Session:
             self.session_date = session_date
         else:
             self.session_date = date.today()
-        if practiced:
-            self.practiced = practiced
-        if improved:
-            self.improved = improved
-        if future:
-            self.future = future
+        self.practiced = practiced
+        self.improved = improved
+        self.future = future
         if file_path:
             self.file_path = file_path
         else:
@@ -45,18 +45,25 @@ class Session:
         if self.has_save_file():
             with open(self.file_path) as f:
                 root = etree.parse(f)
-            if not root.find('skill[@name="{}"]'.format(self.skill_name)):
+            skill_node = root.find('skill[@name="{}"]'.format(self.skill_name))
+            if skill_node is not None:
+                session_node = self.find_existing_session(skill_node)
+                if session_node is not None:
+                    session_node = self.update_xml(session_node)
+                    return root
+            else:
                 skill_node = etree.SubElement(root, 'skill',
                                               name=self.skill_name)
         else:
             root = etree.Element('sessions')
-            skill_node = etree.SubElement(root, 'skill', name=self.skill_name)
+            skill_node = etree.SubElement(root, 'skill',
+                                          name=self.skill_name)
 
-        session_node = etree.SubElement(skill_node, 'session')
-        date_node = etree.SubElement(session_node, 'date')
-        date_node.text = str(self.session_date)
-        time_node = etree.SubElement(session_node, 'hours')
-        time_node.text = str(self.hours)
+        session_node = etree.SubElement(skill_node, 'session',
+                                        date=str(self.session_date))
+        if self.hours:
+            hours_node = etree.SubElement(session_node, 'hours')
+            hours_node.text = str(self.hours)
         if self.practiced:
             practiced_node = etree.SubElement(session_node, 'practiced')
             practiced_node.text = self.practiced
@@ -66,8 +73,33 @@ class Session:
         if self.future:
             future_node = etree.SubElement(session_node, 'future')
             future_node.text = self.future
-
         return root
+
+    def update_xml(self, session_node, separator='<br/>'):
+        """Update XML for existing session"""
+        hours_node = session_node.find('hours')
+        hours_node.text = str(int(hours_node.text) + self.hours)
+        detail_tuples = [('practiced', self.practiced),
+                         ('improved', self.improved),
+                         ('future', self.future)]
+        for detail in detail_tuples:
+            if detail[1]:
+                detail_node = session_node.find(detail[0])
+                if detail_node is None:
+                    detail_node = etree.SubElement(session_node, detail[0])
+                if detail_node.text is not None:
+                    detail_node.text = separator.join([detail_node.text,
+                                                       detail[1]])
+                else:
+                    detail_node.text = detail[1]
+        return session_node
+
+    def find_existing_session(self, skill_node):
+        """Returns the session node if data has already been logged,
+        otherwise returns None."""
+        if skill_node is None:
+            return None
+        return skill_node.find('session[@date="{}"]'.format(self.session_date))
 
     def save(self, root):
         """Save XML tree to file."""
