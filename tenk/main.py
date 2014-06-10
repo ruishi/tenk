c70793d194a8b6dd1712374b602b07b019a0baff
ruishi <ganruishi@gmail.com>
2014-06-10 01:10:23 -0400

    overhaul CLI
    Major overhaul of CLI to use subparsers for a logical grouping of
    commands and more sensible CLI commands.

--- tenk/main.py
+++ tenk/main.py
@@ -9,6 +9,7 @@ import functools
 
 from tenk.users import User
 import tenk.tkserializer as tkserializer
+from tenk.sessions import Session
 
 no_data_msg = "You have no data!"
 
@@ -50,12 +51,17 @@ def generate_dict(xs):
     a number and the value is an element of xs"""
     return {float(i + 1):xs[i] for i in range(len(xs))}
 
-def add_time(choice, time):
+def add_time(choice, time, date=None):
     user = load_user(create=False)
     if user:
         skill = generate_dict(user.get_skill_names())[choice]
         user.add_time(skill, time)
         save(user)
+        params = {'skill_name': skill, 'hours': time}
+        if date:
+            params['session_date'] = date
+        session = Session(**params)
+        session.serialize_and_save()
     else:
         print(no_data_msg)
 
@@ -73,7 +79,17 @@ def remove_skill(choice):
     else:
         print(no_data_msg)
 
+def add_notes(choice, notes):
+    user = load_user(create=False)
+    if user:
+        skill = generate_dict(user.get_skill_names())[choice]
+        session = Session(skill, **notes)
+        session.serialize_and_save()
+    else:
+        print(no_data_msg)
+
 def list_skills():
+    """List a user's skills and each skill's number."""
     user = load_user()
     if user:
         for idx, skill in enumerate(user.get_skill_names()):
@@ -89,41 +105,102 @@ def print_progress():
     else:
         print(no_data_msg)
 
-if __name__ == '__main__':
-    parser  = argparse.ArgumentParser(prog="python3 -m tenk.main")
-    parser.add_argument('-l', '--ls',
-                        action='store_true',
-                        help="List skillset")
-    parser.add_argument('-s', '--skill',
-                        nargs='+',
-                        metavar=('name', 'hours'),
-                        help="Add skill to skillset")
-    parser.add_argument('-t', '--time',
-                        nargs=2,
-                        type=float,
-                        metavar=('skill', 'time'),
-                        help=("Add time to a skill"))
-    parser.add_argument('-d', '--delete',
-                        type=float,
-                        metavar='skill',
-                        help="Delete a skill")
-    parser.add_argument('-p', '--progress',
-                        action='store_true',
-                        help="Print progress")
-    args = parser.parse_args()
-
-    if args.ls:
+def cli_list_handler(args):
+    """Handles list-related CLI arguments"""
+    if args.skills:
         list_skills()
     elif args.progress:
         print_progress()
-    elif args.time:
-        add_time(args.time[0], float(args.time[1]))
+
+def cli_add_handler(args):
+    """Handles add-related CLI arguments"""
+    if args.time:
+        if args.date:
+            add_time(args.time[0], float(args.time[1]), args.date)
+        else:
+            add_time(args.time[0], float(args.time[1]))
     elif args.skill:
         if len(args.skill) == 2:
-            add_skill(args.skill[0], args.skill[1])
+            add_skill(args.skill[0], float(args.skill[1]))
         else:
             add_skill(args.skill[0])
-    elif args.delete:
-        remove_skill(args.delete)
-    else:
-        parser.print_help()
+
+def cli_del_handler(args):
+    """Handles deletion-related CLI arguments"""
+    if args.skill:
+        remove_skill(args.skill)
+
+def cli_note_handler(args):
+    """Handles note-related CLI arguments"""
+    notes = dict()
+    if args.practiced:
+        notes['practiced'] = ' '.join(args.practiced)
+    if args.improved:
+        notes['improved'] = ' '.join(args.improved)
+    if args.future:
+        notes['future'] = ' '.join(args.future)
+    if args.date:
+        notes['session_date'] = args.date
+    if notes:
+        add_notes(args.skill, notes)
+
+if __name__ == '__main__':
+    parser  = argparse.ArgumentParser(prog="python3 -m tenk.main")
+    subparsers = parser.add_subparsers(title='subcommands')
+
+    parser_list = subparsers.add_parser('list', aliases=['ls'])
+    parser_list.add_argument('-s', '--skills',
+                             action='store_true',
+                             help="List skillset")
+    parser_list.add_argument('-p', '--progress',
+                             action='store_true',
+                             help="Print progress")
+    parser_list.set_defaults(func=cli_list_handler)
+
+    parser_add = subparsers.add_parser('add', aliases=['a'])
+    parser_add.add_argument('-s', '--skill',
+                            nargs='+',
+                            metavar=('name', 'hours'),
+                            help="Add skill to skillset")
+    parser_add.add_argument('-d', '--date',
+                            type=str,
+                            help="Custom date: YYYY-MM-DD")
+    parser_add.add_argument('-t', '--time',
+                            nargs=2,
+                            type=float,
+                            metavar=('skill', 'time'),
+                            help=("Add time to a skill"))
+    parser_add.set_defaults(func=cli_add_handler)
+
+    parser_del = subparsers.add_parser('delete', aliases=['del', 'd'])
+    parser_del.add_argument('-s', '--skill',
+                            type=float,
+                            metavar='skill',
+                            help="Delete a skill")
+    parser_del.set_defaults(func=cli_del_handler)
+
+    parser_note = subparsers.add_parser('note', aliases=['n'])
+    parser_note.add_argument('-s', '--skill',
+                             type=float,
+                             metavar='skill',
+                             required=True,
+                             help="Skill to add note to")
+    parser_note.add_argument('-d', '--date',
+                             type=str,
+                             help="Custom date: YYYY-MM-DD")
+    parser_note.add_argument('-p', '--practiced',
+                             nargs='+',
+                             metavar='notes',
+                             help="Note details about what was practiced")
+    parser_note.add_argument('-i', '--improved',
+                             nargs='+',
+                             metavar='notes',
+                             help="Note details about what improved")
+    parser_note.add_argument('-f', '--future',
+                             nargs='+',
+                             metavar='notes',
+                             help="Note focus of next practice")
+    parser_note.set_defaults(func=cli_note_handler)
+
+    cli_args = parser.parse_args()
+    cli_args.func(cli_args)
