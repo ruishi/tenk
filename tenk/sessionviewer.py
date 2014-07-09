cc2c5d4b001c157aafc5a34c0459777ddbf91abf
ruishi <ganruishi@gmail.com>
2014-07-08 23:01:37 -0400

    some bug fixes in sessionviewer
    tenk/sessionviewer.py: handle cases where session data does not exist or
    the skill does not exist.

--- tenk/sessionviewer.py
+++ tenk/sessionviewer.py
@@ -14,18 +14,33 @@ class SessionViewer:
     def get_session(self, date):
         """Returns session data for given date if it exists. Otherwise returns
         None."""
-        return self.subtree.find('session[@date="{}"]'.format(date))
+        if self.subtree is None:
+            print("{} is not in your skillset".format(self.skill))
+            return None
+        else:
+            return self.subtree.find('session[@date="{}"]'.format(date))
 
     def get_recent(self):
-        return self.subtree.findall('session')[-5:]
+        """Returns data for up to the last 5 recent sessions"""
+        if self.subtree is None:
+            print("{} is not in your skillset".format(self.skill))
+            return None
+        else:
+            return self.subtree.findall('session')[-5:]
 
     def print_session(self, date):
         session_node = self.get_session(date)
-        session = Session.deserialize(self.skill, session_node)
-        print(session)
+        if session_node is not None:
+            session = Session.deserialize(self.skill, session_node)
+            print(session)
+        else:
+            print("No session data for that date exists!")
 
     def print_recent(self):
         recent_session_nodes = self.get_recent()
-        for session_node in recent_session_nodes:
-            session = Session.deserialize(self.skill, session_node)
-            print(session)
+        if recent_session_nodes:
+            for session_node in recent_session_nodes:
+                session = Session.deserialize(self.skill, session_node)
+                print(session)
+        else:
+            print("No session data exists for {}".format(self.skill))
