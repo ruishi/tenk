3ac3a07e29f9321e2bebdbe62d225880ada03d80
ruishi <ganruishi@gmail.com>
2014-07-14 01:30:50 -0400

    revamp utils.py and update other files accordingly
    tenk/utils.py: It turns out load_user and save were the only functions
    that are useful to both the CLI and GUI clients. The rest of the
    functions formerly in utils.py overlooked the difference in object
    persistence between the two interfaces.
    tkcli/main.py: Rejected utils.py functions moved here.
    tests/utils_test.py:
    tests/cli_test.py: Unit tests moved/updated accordingly based on changes
    to utils.py.

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -12,24 +12,8 @@ class UtilsTest(unittest.TestCase):
         """load_user() should return None if no data exists and create is
         False"""
         mock_exists.return_value = None
-
-        self.assertIsNone(tenk.utils.load_user())
-
-    @mock.patch('builtins.print')
-    @mock.patch('tenk.utils.load_user')
-    def test_no_user(self, mock_load, mock_print):
-        """If no user exists, a message about no data should be printed
-        for add_time(), remove_skill(), add_notes()"""
-        no_data_msg = "You have no data!"
-        mock_load.return_value = False
-        tenk.utils.add_time('piano', 1.0)
-        mock_print.assert_called_with(no_data_msg)
-
-        tenk.utils.remove_skill('programming')
-        mock_print.assert_called_with(no_data_msg)
-
-        tenk.utils.add_notes('programming', {})
-        mock_print.assert_called_with(no_data_msg)
+        config = {'PATHS': {'tk_dir': '/nonexistent_dir'}}
+        self.assertIsNone(tenk.utils.load_user(config=config))
 
 if __name__ == '__main__':
     unittest.main()
